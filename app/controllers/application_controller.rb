b488857080e5b7c72469abbd4a4e3a69c8218d88
you0626 <sy0626sk@icloud.com>
2024-10-03 01:31:58 +0900

    ログアウトまで一時的にOK

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,8 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
+
+  def after_sign_out_path_for(resource_or_scope)
+    users_path
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!, only: [:index]
+  before_action :authenticate_user!, except: [:index]
   before_action :update_user_last_login_at, if: :user_signed_in?
 
   def index
