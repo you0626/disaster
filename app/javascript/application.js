f2305216b59a380658f8bece9c28afae203510c6
you0626 <sy0626sk@icloud.com>
2024-10-07 12:40:49 +0900

    完了

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,27 +3,22 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails";
 import Rails from "@rails/ujs";
-import * as ActiveStorage from "@rails/storage";
+import * as ActiveStorage from "@rails/activestorage";
 import "channels";
-import "jquery"; // jQueryをインポート
-import "bootstrap"; // Bootstrapをインポート
+import "packs/application";
 
 // Stimulusのインポート
 import { Application } from "@hotwired/stimulus";
 import DropdownController from "./controllers/dropdown_controller"; 
-import HelloController from "./controllers/hello_controller"; // 他のコントローラのインポート
-import SomeController from "./controllers/some_controller"; // 新しいコントローラのインポート
+import HelloController from "./controllers/hello_controller";
+import SomeController from "./controllers/some_controller";
 
-// Rails UJSの初期化
+// Rails UJSとActiveStorageの初期化
 Rails.start();
 ActiveStorage.start();
 
 // Stimulusの初期化
 const application = Application.start();
 application.register("dropdown", DropdownController);
-application.register("hello", HelloController); // 他のコントローラの登録
-application.register("some", SomeController); // 新しいコントローラの登録
-
-// Controller registration can be done in a loop if there are many controllers
-// const controllers = { dropdown: DropdownController, hello: HelloController, some: SomeController };
-// Object.entries(controllers).forEach(([name, controller]) => application.register(name, controller));
+application.register("hello", HelloController);
+application.register("some", SomeController);
