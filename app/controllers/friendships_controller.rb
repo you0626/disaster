fe28da305e8e930c12e080d732743beb9382e315
you0626 <sy0626sk@icloud.com>
2024-10-10 00:34:36 +0900

    messageテーブルを作成して作り直す

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -2,22 +2,64 @@ class FriendshipsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @friendships = current_user.friendships
+    @friends = current_user.friends  # ユーザーのフレンドを取得
+    @search_results = []              # 検索結果を初期化
+    @friendships = Friendship.where(user_id: current_user.id)
+    @user = current_user
+    
+    # フラッシュメッセージを格納
+    if flash[:notice].present? && flash[:notice] != "Signed in successfully."
+      @messages = [flash[:notice]]  # メッセージがあれば配列に格納
+    else
+      @messages = []  # メッセージがない場合は空の配列
+    end
+
+    if params[:name].present?
+      @search_results = User.where("name LIKE ?", "%#{params[:name]}%").where.not(id: current_user.id) # ユーザー名で検索
+    end
+    
   end
 
   def create
-    @friendship = current_user.friendships.new(friendship_params)
+    @friendship = Friendship.new(user_id: current_user.id, friend_id: params[:friend_id])
+  
     if @friendship.save
-      redirect_to friendships_path, notice: '友達リクエストが送信されました。'
+      # フレンド登録成功時のメッセージ
+      @messages = Friendship.where(friend_id: current_user.id).map do |friendship|
+        "#{User.find(friendship.user_id).name}さんがあなたをフレンド登録しました。"
+      end
+      redirect_to user_friendships_path(current_user)
     else
-      redirect_to friendships_path, alert: '友達リクエストの送信に失敗しました。'
+      flash[:alert] = "フレンド登録に失敗しました。"
+      redirect_to user_friendships_path(current_user)
     end
   end
 
   def destroy
-    @friendship = current_user.friendships.find(params[:id])
-    @friendship.destroy
-    redirect_to friendships_path, notice: '友達関係が削除されました。'
+    friendship = Friendship.find(params[:id])
+    if friendship.destroy
+      flash[:notice] = "フレンドを解除しました。"
+    else
+      flash[:alert] = "フレンド解除に失敗しました。"
+    end
+    redirect_to user_friendships_path(current_user) # 適切なリダイレクト先
+  end
+
+  def search
+    @user = User.find(params[:user_id]) # ユーザーを取得
+    @friendships = Friendship.where(user_id: @user.id) # ユーザーのフレンドシップを取得
+  
+    # 名前でフレンドを検索
+    if params[:name].present?
+      @friends = User.where("name LIKE ?", "%#{params[:name]}%").where.not(id: current_user.id)
+    else
+      @friends = [] # 検索結果を空にする
+    end
+  
+    # 既存のフレンドリストを取得
+    @existing_friends = current_user.friends
+  
+    render :index # indexテンプレートをレンダリング
   end
 
   private
